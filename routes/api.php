c5a0562f70d40f2b50e4751e2bfb16d2f23bc569
master-8702 <55705389+master-8702@users.noreply.github.com>
2023-01-30 15:26:27 +0300

    add CRUD operation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Food;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,69 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/// a route for getting all popular food items
+Route::get('/foods/popular', function(){
+    return Food::all();
+});
+
+/// a route for getting all popular food items
+Route::get('/foods/popular/{id}', function( $hij){
+    return Food::find($hij);
+});
+
+
+
+/// a route for posting a new popular food item
+
+Route::post('/foods/popular', function(){
+
+    /// here we will create a new popular food record in the database
+    /// and return the new record
+    return Food::create([
+
+        'id' => request('id'),
+        'name' => request('name'),
+        'description' => request('description'),
+        'price' => request('price'),
+        'stars' => request('stars'),
+        'image' => request('image'),
+        'location' => request('location'),
+        'type_id' => request('type_id'),
+    ]);
+});
+
+
+/// a route for updating apopular food item
+
+Route::put('/foods/popular/{food}', function(Food $food){
+
+   /// we will update the record here
+
+   $success =  $food->update([
+
+        'id' => request('id'),
+        'name' => request('name'),
+        'description' => request('description'),
+        'price' => request('price'),
+        'stars' => request('stars'),
+        'image' => request('image'),
+        'location' => request('location'),
+        'type_id' => request('type_id'),
+    ]);
+/// and we will return a success message here
+
+    return ['success' => $success];
+});
+
+
+// a route for deleting apopular food item
+
+Route::delete('/foods/popular/{food}', function(Food $food){
+   /// we will delete it here from the database
+    $success =  $food->delete();
+
+/// and we will return a success message here
+     return ['success' => $success];
+ });
+ 
